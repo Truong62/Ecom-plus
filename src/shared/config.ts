6a9910ace9d3f91ec233b72298d79d1b25d8a1f1
Truong62 <truongnn@avadagroup.com>
2026-03-10 10:31:14 +0700

    update env

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -17,13 +17,16 @@ const configSchema = z.object({
   ACCESS_TOKEN_EXPIRES_IN: z.string(),
   REFRESH_TOKEN_SECRET: z.string(),
   REFRESH_TOKEN_EXPIRES_IN: z.string(),
-
   ADMIN_EMAIL: z.string(),
   ADMIN_PASSWORD: z.string(),
   ADMIN_NAME: z.string(),
   ADMIN_PHONE_NUMBER: z.string(),
   OTP_EXPIRES_IN: z.string(),
   RESEND_API_KEY: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  GOOGLE_REDIRECT_URI: z.string(),
+  GOOGLE_CLIENT_REDIRECT_URI: z.string(),
 });
 
 const configService = configSchema.safeParse(process.env);
